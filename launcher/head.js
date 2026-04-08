4d4cfa0abd92d97e2b8eb3b0526ef4ed7cede3fa
01a0ef2e-8b00-79b1-8f3e-c259c179dac0 <taylor.paddley@gmail.com>
2026-04-08 08:01:19 +0200

    turn off bookmarklet

--- launcher/head.js
+++ launcher/head.js
@@ -1,4 +1,4 @@
-javascript:(async function () {
+/*javascript:*/(async function () {
   const baseUrl = "https://ceb640a6-cb7e-45e9-aaf2-fbaefc740186.github.io/019cfb6d-c100-7f9c-9735-18986e56a4eb/";
   const files = ["headings.js", "links.js"];
   const results = [];
