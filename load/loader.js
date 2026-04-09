3dc9ffd4eaa7ab408a68536eb28f47f5346d3637
01a0ef2e-8b00-79b1-8f3e-c259c179dac0 <taylor.paddley@gmail.com>
2026-04-09 11:22:28 +0200

    Update loader.js

--- load/loader.js
+++ load/loader.js
@@ -320,6 +320,7 @@
             Pass
             <strong>${summary.pass}</strong>
           </div>
+          <canvas id="summary-chart" width="400" height="400"></canvas>
         </div>
 
         ${results.map(r => `
@@ -331,6 +332,49 @@
             <div class="box-content">${r.content}</div>
           </div>
         `).join("")}
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+          const ctx = document.getElementById('summary-chart').getContext('2d');
+          const data = {
+            labels: ['Green', 'Yellow', 'Gray', 'Red'],
+            datasets: [{
+              label: 'My Dataset',
+              data: [${summary.pass}, ${summary.check}, ${summary.neutral}, ${summary.fail}], // your four values
+              backgroundColor: [
+                ${getRootVar('--pass-dark')},
+                ${getRootVar('--check-dark')},
+                ${getRootVar('--neutral-dark')},
+                ${getRootVar('--fail-dark')}
+              ],
+              borderColor: [
+                '#ffffff00',
+                '#ffffff00',
+                '#ffffff00',
+                '#ffffff00'
+              ],
+              borderWidth: 2
+            }]
+          };
+
+          const config = {
+            type: 'doughnut',
+            data: data,
+            options: {
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: 'top',
+                },
+                tooltip: {
+                  enabled: true
+                }
+              }
+            }
+          };
+
+          new Chart(ctx, config);
+        </script>
       </body>
       </html>
     `;
@@ -347,6 +391,10 @@
       .replaceAll(">", "&gt;")
       .replaceAll('"', "&quot;")
       .replaceAll("'", "&#039;");
+  }
+
+  function getRootVar(str) {
+    return window.getComputedStyle(document.body).getPropertyValue(str);
   }
 
   async function init() {
