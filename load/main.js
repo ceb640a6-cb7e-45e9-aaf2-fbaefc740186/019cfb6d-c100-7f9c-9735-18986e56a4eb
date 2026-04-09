4a488e2a885b520df9d34f396dcf3afe93e8a5ab
01a0ef2e-8b00-79b1-8f3e-c259c179dac0 <taylor.paddley@gmail.com>
2026-04-09 13:18:05 +0200

    Update main.js

--- load/main.js
+++ load/main.js
@@ -405,7 +405,7 @@ const tests = {
           selector += `.${Array.from(el.classList).slice(0, 4).join(".")}`;
         }
 
-        results.push(`${selector || "(node)"}: ${matches.join(" | ")}`);
+        results.push(`${selector || "(node)"}: ${matches.join(" | ")}<br>Position: ${getDomPath(el)}`);
       }
     });
 
@@ -422,7 +422,7 @@ const tests = {
       title: "CSS-Text in Pseudo-Elementen",
       status: "fail",
       content:
-        `Es wurden ${results.length} Element(e) mit per CSS eingebundenem Text gefunden:\n\n` +
+        `Es wurden ${results.length} Element(e) mit per CSS eingebundenem Text gefunden:<br>` +
         results.join("\n")
     };
   },
