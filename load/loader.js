beae8f2c46f4513493aa326d6e08bdd90c236010
01a0ef2e-8b00-79b1-8f3e-c259c179dac0 <taylor.paddley@gmail.com>
2026-04-12 07:46:16 +0200

    Update loader.js

--- load/loader.js
+++ load/loader.js
@@ -388,7 +388,7 @@
               <h2 class="toggleText">${escapeHtml(r.title)}</h2>
               <span class="badge badge-${r.status}">${getBadgeLabel(r.status)}</span>
             </summary>
-            <div class="box-content"
+            <div class="box-content">
               ${r.content}
               <small class="reqId">${escapeHtml(r.id)}</small>
             </div>
